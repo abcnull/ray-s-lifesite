4f610a18e0959242eccffa335bec7044a4f20780
abcnull <abcnull@qq.com>
2025-12-15 02:55:39 +0800

    update github project content

--- src/pages/projects/data.ts
+++ src/pages/projects/data.ts
@@ -6,13 +6,13 @@ export const projectData: ProjectData = [
     projects: [
       {
         text: 'basic_springboot_demo',
-        description: '学习时候，使用 java 结合 springboot 框架搭的一个粗糙的 web 草稿学习项目',
+        description: 'springboot_demo 草稿 demo',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/basic_springboot_demo',
       },
       {
         text: 'gindemo',
-        description: '学习时候，使用 golang 结合 gin 框架搭的一个粗糙的 web 草稿学习项目',
+        description: 'golang + gin 框架 Web 草稿 demo',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/gindemo',
       },
@@ -21,7 +21,14 @@ export const projectData: ProjectData = [
 
   {
     title: 'Web 前端项目 Web Frontend',
-    projects: [],
+    projects: [
+      {
+        text: 'html-and-css',
+        description: 'html + css 入门教学多种 css 样式 demo',
+        icon: 'i-ri-github-line',
+        href: 'https://github.com/abcnull/html-and-css',
+      },
+    ],
   },
 
   {
@@ -29,7 +36,7 @@ export const projectData: ProjectData = [
     projects: [
       {
         text: 'MessageWallApp',
-        description: '很粗糙的短信拦截手机短信的学习小项目',
+        description: 'java + android sdk 粗糙的短信拦截手机短信学习小项目',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/MessageWallApp',
       },
@@ -41,13 +48,13 @@ export const projectData: ProjectData = [
     projects: [
       {
         text: 'godot-mini-games-demo',
-        description: '使用 godot 和 gdscript 复刻的各种经典的小游戏',
+        description: 'gdscript + godot 引擎复刻各种经典小游戏',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/godot-mini-games-demo',
       },
       {
         text: 'IndependentPlaneGame',
-        description: '使用 java 和 android 相关 sdk 开发的飞机大战小游戏',
+        description: 'java + android sdk 飞机大战小游戏',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/IndependentPlaneGame',
       },
@@ -59,31 +66,31 @@ export const projectData: ProjectData = [
     projects: [
       {
         text: 'apitest4j',
-        description: '使用 java 结合 RESTassured 和 testng 的一个接口自动化 demo 项目',
+        description: 'java + RESTassured + testng 接口自动化 demo',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/apitest4j',
       },
       {
         text: 'api-auto-test',
-        description: '使用 java 结合 httpclient 和 testng 的一个接口自动化 demo 项目',
+        description: 'java + httpclient + testng 接口自动化 demo',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/api-auto-test',
       },
       {
         text: 'webuitest4j',
-        description: '使用 java 结合 selenium 和 testng 和 allure2 的一个接口自动化 demo 项目',
+        description: 'java + selenium + testng + allure2 Web UI 自动化 demo',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/webuitest4j',
       },
       {
         text: 'python-ui-auto-test',
-        description: '使用 python 结合 selenium 和 unittest 和 BeautifulReport 的一个接口自动化 demo 项目',
+        description: 'python + selenium + unittest + BeautifulReport Web UI 自动化 demo',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/python-ui-auto-test',
       },
       {
         text: 'appuitest4j',
-        description: '使用 java 结合 appium 和 testng 和 allure2 的一个接口自动化 demo 项目',
+        description: 'java + appium + testng + allure2 APP UI 自动化 demo',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/appuitest4j',
       },
@@ -95,7 +102,7 @@ export const projectData: ProjectData = [
     projects: [
       {
         text: 'lottery4bash',
-        description: 'shell 写的一个抽奖小脚本',
+        description: 'shell 抽奖小脚本',
         icon: 'i-ri-github-line',
         href: 'https://github.com/abcnull/lottery4bash',
       },
